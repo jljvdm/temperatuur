dcb25164bf67297b699687288d52d047e9be53a0
Jeroen van der Meer sr <jeroen.van.der.meer@gmail.com>
2014-12-08 12:28:27 +0000

    De juiste categorien in configuratie toegevoegd. Typo's verbeterd

--- collector/collector.js
+++ collector/collector.js
@@ -2,4 +2,5 @@ var http = require('http');
 var fs = require("fs");
 var ini = require("ini");
 
-var config = ini.parse(fs.readFileSync('./collector.ini', 'utf-8'))
+var config = ini.parse(fs.readFileSync('./collector/collector.ini', 'utf-8'));
+console.log(config);
